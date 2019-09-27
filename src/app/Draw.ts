38e858da73ff5203c31f9f3d905e518a0c8e3292
Alexander Baladin <my@maggit.ru>
2019-09-27 16:46:35 +0300

    WIP

--- src/app/Draw.ts
+++ src/app/Draw.ts
@@ -1,13 +1,53 @@
-import p5 from 'p5'
+import * as p5 from 'p5'
 
-const radius = 50
+const radius = 20
 const diameter = radius * 2
+
+const circles = []
+
+console.log(p5)
+
+const checkIfInside = (
+  cursor, center
+) => {
+  console.log(cursor, center - radius, center + radius)
+  return (cursor < center - radius && cursor < center + radius)
+}
 
 const Draw = (p: p5) => {
   p.background(0)
-  p.fill(255)
 
-  p.circle(p.mouseX, p.mouseY, diameter)
+  if (p.mouseIsPressed) {
+    console.log(p.mouseClicked)
+    console.log(p)
+  }
+  // p.mouseClicked(e => {
+  //   const index = circles.findIndex(circle => {
+  //     console.log(checkIfInside(p.mouseX, circle.x) && checkIfInside(p.mouseY, circle.y))
+  //     return checkIfInside(p.mouseX, circle.x) && checkIfInside(p.mouseY, circle.y)
+  //   })
+
+  //   const coords = {
+  //     x: p.mouseX,
+  //     y: p.mouseY
+  //   }
+
+  //   if (index === -1) {
+  //     circles.push(coords)
+  //   } else {
+  //     circles[index] = coords
+  //   }
+  // })
+
+  circles.forEach(circle => {
+    if (p.mouseIsPressed) {
+      p.fill(204, 102, 0)
+    } else {
+      p.fill(255)
+    }
+
+    p.circle(circle.x, circle.y, diameter)
+  })
 }
 
 export default Draw
