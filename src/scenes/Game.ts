a87e93edde3dcdee0c35102f435bbfcff0d2687e
Alexander Balandin <my@maggit.ru>
2019-10-08 21:19:05 +0300

    Move player without x axis inertial

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -17,20 +17,23 @@ export class Game extends Phaser.Scene {
   public create () {
     this.matter.world.setBounds()
 
-    this.player = this.add.rectangle(50, 50, 60, 60, 0x555599ff)
+    this.player = this.add.ellipse(50, 50, 60, 60, 0x555599ff)
     this.matter.add.gameObject(this.player, {
-      // chamfer: { radius: 30 },
-      density: 0.05,
+      chamfer: { radius: 30 },
+      restitution: 0,
+      density: 0.03,
       frictionAir: 0,
-      friction: 5
+      friction: 0,
+      frictionStatic: 0
       // timeScale: 3
     })
 
     this.ball = this.add.ellipse(400, 100, 60, 60, 0x55ff5555)
     this.matter.add.gameObject(this.ball, {
       chamfer: { radius: 30 },
+      density: 0.02,
       restitution: 1,
-      frictionAir: 0.01,
+      frictionAir: 0,
       friction: 0,
       timeScale: 0.5
     })
@@ -41,27 +44,25 @@ export class Game extends Phaser.Scene {
     this.matter.add.gameObject(this.separator, { isStatic: true })
 
     this.matter.add.mouseSpring({})
-    console.log(this.ball.body)
+    console.log(this.ball)
   }
 
   public update () {
     const cursors = this.input.keyboard.createCursorKeys()
 
-    // if (this.ball.body.speed > 10) {
-    //   this.ball.body.set()
-    // }
-
     if (cursors.left.isDown) {
       this.player.setVelocityX(-PLAYER_SPEED)
     } else if (cursors.right.isDown) {
       this.player.setVelocityX(PLAYER_SPEED)
-    } else {
-      // const velocityX = this.player.velocity.x
+    } else if (this.player.body.velocity.x) {
+      this.player.setVelocityX(0)
     }
 
     if (cursors.up.isDown) {
     // if (cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-PLAYER_SPEED - 10)
+      this.player.setVelocityY(-PLAYER_SPEED)
+    } else if (this.player.body.velocity.y < -2) {
+      this.player.setVelocityY(0)
     }
 
     if (cursors.down.isDown) {
